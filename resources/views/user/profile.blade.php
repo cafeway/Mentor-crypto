3f0b80b45587893efca142c2a856b0e2088c4472
cafeway <bcaranja@gmail.com>
2021-12-01 12:55:13 +0300

    ..|

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -44,7 +44,7 @@
                     <div class="container-fluid">
                         <!-- begin row -->
                         <div class="row">
-                            <div class="col-md-12 m-b-30">
+                            <div class="col-md-12 m-b-29">
                                 <!-- begin page title -->
                                 <div class="d-block d-sm-flex flex-nowrap align-items-center">
                                     <div class="page-title mb-2 mb-sm-0">
